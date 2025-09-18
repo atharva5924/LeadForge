5fae50030345e08b89fc91ae9c1338246e72dcdc
Atharva Nile <atharvanile2005@gmail.com>
2025-09-19 00:51:28 +0530

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -35,7 +35,7 @@ const registerUser = async (req, res) => {
     // Set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
