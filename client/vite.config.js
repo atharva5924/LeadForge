eeade17dd6abfc671ab71854bb7bd787acfe2a4d
atharva-nile <atharva@intandem.co.in>
2025-09-18 19:44:57 +0530

    Production changes (Backend URL).

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://leadforge-backend.onrender.com",
         changeOrigin: true,
       },
     },
