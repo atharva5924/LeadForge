cd3c356f9175934175ada83c761f6509e90d5a2f
Atharva Nile <atharvanile2005@gmail.com>
2025-09-19 00:56:59 +0530

    Production Cookie Setting Change

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -84,8 +84,8 @@ const loginUser = async (req, res) => {
     // Set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
